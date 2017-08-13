b1bbab1b4b12517876b1bdf5e10117f4a988c825
wgarvie <wegarvie@gmail.com>
2017-08-12 21:52:37 -0500

    Drawing all chess pieces.

--- public/js/chess-client.js
+++ public/js/chess-client.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   const canvas = $('.game-canvas').get(0);
   const context = canvas.getContext('2d');
-  //let blackPawn = new Pawn("black");
-  //blackPawn.drawPiece(context);
   const board = new Board;
   board.drawBoard(context);
 });
