155176e45a05c204cebe163f04f5c29909426e26
wgarvie <wegarvie@gmail.com>
2017-08-13 12:10:02 -0500

    Basic piece move animation

--- public/js/chess-client.js
+++ public/js/chess-client.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
   const canvas = $('.game-canvas').get(0);
   const context = canvas.getContext('2d');
-  const board = new Board;
-  board.drawBoard(context);
+  let chessBoard = new ChessBoard;
+  let heldPiece = null;
+  canvas.addEventListener('mousedown', clientMouseDown);
+  canvas.addEventListener('mousemove', clientMouseMove);
+  setInterval(function() {
+    mainLoop();
+  }, 50);
+
+  function mainLoop() {
+    chessBoard.drawBoard(context);
+  }
+
+  function clientMouseDown(e) {
+    heldPiece = chessBoard.board[Math.floor(e.y / chessBoard.tileSize)][Math.floor(e.x / chessBoard.tileSize)];
+  }
+
+  function clientMouseMove(e) {
+    if(heldPiece!=null){
+      heldPiece.xPos = (e.x - (heldPiece.width / 2));
+      heldPiece.yPos = (e.y - (heldPiece.height / 2));
+      console.log(heldPiece.xPos);
+    }
+  }
+
 });
--- public/js/chess-objects.js
+++ public/js/chess-objects.js
@@ -1,39 +1,40 @@
-Board = function() {
+ChessBoard = function() {
   this.boardHeight = 600;
   this.tileSize = this.boardHeight / 8;
   this.darkTileColor = "#B29977";
   this.lightTileColor = "#FFE5C3";
   this.board = new Array(8);
   for(let i = 0; i < this.board.length; i++) {
-    this.board[i] = new Array(8);
+    this.board[i] = new Array(8).fill(null);
   }
-  //initializing black and white pieces
   for(let x = 0; x < 8; x++) {
-    this.board[1][x] = new Pawn("black", (75*x)+2, (75*1)+2);
-    this.board[6][x] = new Pawn("white", (75*x)+2, (75*6)+2);
+    const xPos = (75*x)+2;
+    let yPos = y => (75 * y) + 2;
+    this.board[1][x] = new Piece("pawn", "black", xPos, yPos(1));
+    this.board[6][x] = new Piece("pawn", "white", xPos, yPos(6));
     switch(x) {
       case 0:
       case 7:
-        this.board[0][x] = new Rook("black", (75*x)+2, (75*0)+2);
-        this.board[7][x] = new Rook("white", (75*x)+2, (75*7)+2);
+        this.board[0][x] = new Piece("rook","black", xPos, yPos(0));
+        this.board[7][x] = new Piece("rook","white", xPos, yPos(7));
         break;
       case 1:
       case 6:
-        this.board[0][x] = new Bishop("black", (75*x)+2, (75*0)+2);
-        this.board[7][x] = new Bishop("white", (75*x)+2, (75*7)+2);
+        this.board[0][x] = new Piece("bishop","black", xPos, yPos(0));
+        this.board[7][x] = new Piece("bishop","white", xPos, yPos(7));
         break;
       case 2:
       case 5:
-        this.board[0][x] = new Knight("black", (75*x)+2, (75*0)+2);
-        this.board[7][x] = new Knight("white", (75*x)+2, (75*7)+2);
+        this.board[0][x] = new Piece("knight","black", xPos, yPos(0));
+        this.board[7][x] = new Piece("knight","white", xPos, yPos(7));
         break;
       case 3:
-      this.board[0][x] = new Queen("black", (75*x)+2, (75*0)+2);
-      this.board[7][x] = new Queen("white", (75*x)+2, (75*7)+2);
+        this.board[0][x] = new Piece("queen","black", xPos, yPos(0));
+        this.board[7][x] = new Piece("queen","white", xPos, yPos(7));
         break;
       case 4:
-      this.board[0][x] = new King("black", (75*x)+2, (75*0)+2);
-      this.board[7][x] = new King("white", (75*x)+2, (75*7)+2);
+        this.board[0][x] = new Piece("king","black", xPos, yPos(0));
+        this.board[7][x] = new Piece("king","white", xPos, yPos(7));
         break;
     }
   }
@@ -43,6 +44,10 @@ Board = function() {
       for(x = 0; x < 8; x+=1) {
         context.fillStyle= ((x+y) % 2 == 0) ? this.lightTileColor: this.darkTileColor;
         context.fillRect(x*this.tileSize,y*this.tileSize, this.tileSize, this.tileSize);
+      }
+    }
+    for (y = 0; y < 8; y+=1) {
+      for(x = 0; x < 8; x+=1) {
         if(this.board[y][x] != null) {
           this.board[y][x].drawPiece(context);
         }
@@ -52,55 +57,17 @@ Board = function() {
 
 }
 
-Piece = function(newColor, xPos, yPos) {
+Piece = function(newType, newColor, xPos, yPos) {
+  this.color = newColor;
+  this.type = newType;
   this.xPos = xPos;
   this.yPos = yPos;
   this.height = 70;
   this.width = 70;
   this.img = new Image();
-  this.color = newColor;
+  this.img.src = "images/" + this.color + "-" + this.type + ".png";
 
   this.drawPiece = function (context) {
-    //callback enclosed code must have the current state of this
-    //bounded to it.
-    this.img.onload = function() {
-      context.drawImage(this.img, this.xPos, this.yPos, this.height, this.width);
-    }.bind(this);
+    context.drawImage(this.img, this.xPos, this.yPos, this.height, this.width);
   }
 }
-
-Pawn = function(newColor, xPos, yPos) {
-  Piece.call(this, newColor, xPos, yPos);
-  this.img.src =  "images/" + this.color + "-pawn.png";
-}
-Pawn.prototype = Object.create(Piece.prototype);
-
-Rook = function(newColor, xPos, yPos) {
-  Piece.call(this, newColor, xPos, yPos);
-  this.img.src =  "images/" + this.color + "-rook.png";
-}
-Rook.prototype = Object.create(Piece.prototype);
-
-Bishop = function(newColor, xPos, yPos) {
-  Piece.call(this, newColor, xPos, yPos);
-  this.img.src =  "images/" + this.color + "-bishop.png";
-}
-Rook.prototype = Object.create(Piece.prototype);
-
-Knight= function(newColor, xPos, yPos) {
-  Piece.call(this, newColor, xPos, yPos);
-  this.img.src =  "images/" + this.color + "-knight.png";
-}
-Knight.prototype = Object.create(Piece.prototype);
-
-Queen= function(newColor, xPos, yPos) {
-  Piece.call(this, newColor, xPos, yPos);
-  this.img.src =  "images/" + this.color + "-queen.png";
-}
-Queen.prototype = Object.create(Piece.prototype);
-
-King= function(newColor, xPos, yPos) {
-  Piece.call(this, newColor, xPos, yPos);
-  this.img.src =  "images/" + this.color + "-king.png";
-}
-King.prototype = Object.create(Piece.prototype);
