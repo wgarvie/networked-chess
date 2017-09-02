1dccfdefe27032bb53da515c8dee3c0059600ce9
wgarvie <wegarvie@gmail.com>
2017-09-02 13:40:20 -0500

    Wrote newBoard function in node.js chess-objects file.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app)
-var io = require('socket.io')(server)
-var port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+//const io = require('socket.io')(server)
+const port = process.env.PORT || 3000;
+
+const chessobjects = require('./chess-objects')
 
 server.listen(port,function() {
-  console.log('Server listening at port ' + port);
-});
+  console.log('Server listening at port ' + port)
+})
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
+
+const board = chessobjects.newBoard(75)
+for(let i = 0; i < board.length; i++) {
+  for(let j = 0; j < board.length; j++) {
+    //checking truthiness
+    if(board[i][j])
+      process.stdout.write(board[i][j].type + " ")
+  }
+  console.log()
+}
