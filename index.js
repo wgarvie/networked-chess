582e8f1d9688c989d74ac3954667faf94f9d2751
wgarvie <wegarvie@gmail.com>
2017-09-03 12:35:14 -0500

    Basic login functionality with random selection of player color.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+//initializing the app
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 const chessobjects = require('./chess-objects')
 
 server.listen(port,function() {
@@ -12,16 +13,43 @@ server.listen(port,function() {
 
 app.use(express.static(__dirname + '/public'))
 
-const board = chessobjects.newBoard(75)
-for(let i = 0; i < board.length; i++) {
-  for(let j = 0; j < board.length; j++) {
-    //checking truthiness
-    if(board[i][j])
-      process.stdout.write(board[i][j].type + " ")
-  }
-  console.log()
-}
-
+//server side game logic
+let numUsers = 0
+let whiteLoggedIn = false
+let blackLoggedIn = false
 io.on('connection', function(client) {
-  console.log("User connected")
+
+  client.on('add user', function(username) {
+    numUsers++
+    client.username = username
+    console.log(client.username + " has logged in.")
+    console.log(numUsers + " users are connected.")
+    if(!whiteLoggedIn && !blackLoggedIn) {
+      const rand = Math.floor(Math.random() * (2));
+      if(rand === 0){
+        client.color = "white"
+        whiteLoggedIn = true
+      } else {
+        client.color = "black"
+        blackLoggedIn = true
+      }
+      console.log(client.username + " has been randomly selected to play as " + client.color + ".")
+    }
+    else if(whiteLoggedIn && blackLoggedIn) {
+      client.color = "spectator"
+      console.log("This game already has two players, so " + client.username + " will be a " + client.color + " for this match.")
+    }
+    else {
+      if(blackLoggedIn){
+        client.color = "white"
+        whiteLoggedIn = true
+      } else {
+        client.color = "black"
+        blackLoggedIn = true
+      }
+      console.log(client.username + " has joined the game second and will play as " + client.color + ".")
+    }
+    //client.emit('newGame', game, client.color)
+  })
+
 })
--- public/js/chess-client.js
+++ public/js/chess-client.js
@@ -2,81 +2,102 @@ $(document).ready(function(){
   const socket = io()
   const canvas = $('.game-canvas').get(0)
   const context = canvas.getContext('2d')
-  let chessGame = new ChessGame
+  var $userNameInput = $('.login-screen__input');
+  var $loginScreen = $('.login-screen');
+
+  function cleanInput (input) {
+    return $('<div/>').text(input.trim()).text();
+  }
+
+  $userNameInput.keydown(function (event) {
+    if(event.which == 13) {
+      setUserName();
+    }
+  });
+
+  function setUserName() {
+    username = cleanInput($userNameInput.val());
+    if(username) {
+      $loginScreen.fadeOut();
+      socket.emit('add user', username);
+    }
+  }
+  
+  /*let chessGame = new ChessGame
   let heldPiece = null
   let heldY = -1
   let heldX = -1
   let turn = "white"
-  let validMoves = chessGame.getValidMoves(turn);
-  canvas.addEventListener('mousedown', clientMouseDown);
-  canvas.addEventListener('mousemove', clientMouseMove);
-  canvas.addEventListener('mouseup', clientMouseUp);
-  canvas.addEventListener('mouseout', clientMouseOut);
+  let validMoves = chessGame.getValidMoves(turn)
+  canvas.addEventListener('mousedown', clientMouseDown)
+  canvas.addEventListener('mousemove', clientMouseMove)
+  canvas.addEventListener('mouseup', clientMouseUp)
+  canvas.addEventListener('mouseout', clientMouseOut)
   setInterval(function() {
-    mainLoop();
-  }, 50);
+    mainLoop()
+  }, 50)
 
   function mainLoop() {
-    chessGame.drawGame(context);
+    chessGame.drawGame(context)
   }
 
   function clientMouseDown(e) {
-    const mouseY = mouseToBoard(e.y);
-    const mouseX = mouseToBoard(e.x);
-    heldPiece = chessGame.board[mouseY][mouseX];
+    const mouseY = mouseToBoard(e.y)
+    const mouseX = mouseToBoard(e.x)
+    heldPiece = chessGame.board[mouseY][mouseX]
     if(heldPiece != null) {
-      heldY = mouseY;
-      heldX = mouseX;
-      heldPiece.held = true;
+      heldY = mouseY
+      heldX = mouseX
+      heldPiece.held = true
     }
   }
 
   function clientMouseMove(e) {
     if(heldPiece!=null){
-      heldPiece.xPos = (e.x - (heldPiece.width / 2));
-      heldPiece.yPos = (e.y - (heldPiece.height / 2));
+      heldPiece.xPos = (e.x - (heldPiece.width / 2))
+      heldPiece.yPos = (e.y - (heldPiece.height / 2))
     }
   }
 
   function clientMouseUp(e) {
-    var mouseX = mouseToBoard(e.x);
-    var mouseY = mouseToBoard(e.y);
+    var mouseX = mouseToBoard(e.x)
+    var mouseY = mouseToBoard(e.y)
     //console.log(mouseToBoard(e).x + " " + mouseToBoard(e).y);
     if(heldPiece!=null){
       if(checkMoveValidity(mouseX, mouseY, heldPiece)) {
-        chessGame.board[heldY][heldX] = null;
-        chessGame.board[mouseY][mouseX] = heldPiece;
-        console.log(chessGame.board);
+        chessGame.board[heldY][heldX] = null
+        chessGame.board[mouseY][mouseX] = heldPiece
+        console.log(chessGame.board)
       }
-      heldPiece.held = false;
-      heldPiece = null;
-      heldY = -1;
-      heldX = -1;
-      chessGame.resetPieces();
+      heldPiece.held = false
+      heldPiece = null
+      heldY = -1
+      heldX = -1
+      chessGame.resetPieces()
     }
   }
 
   function clientMouseOut(e) {
     if(heldPiece!=null){
-      heldPiece.held = false;
-      heldPiece = null;
-      heldY = -1;
-      heldX = -1;
-      chessGame.resetPieces();
+      heldPiece.held = false
+      heldPiece = null
+      heldY = -1
+      heldX = -1
+      chessGame.resetPieces()
     }
   }
 
   function mouseToBoard(mouseLocation) {
-    return Math.floor(mouseLocation / chessGame.tileSize);
+    return Math.floor(mouseLocation / chessGame.tileSize)
   }
 
   function checkMoveValidity(x, y, piece) {
     for(var i = 0; i < validMoves.length; i++) {
       if(x == validMoves[i].x && y ==validMoves[i].y && piece==validMoves[i].piece) {
-        return true;
+        return true
       }
     }
-    return false;
-  }
+    return false
+  }*/
 
 });
